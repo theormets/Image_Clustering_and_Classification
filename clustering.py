cf3b19d1fdcb8448134bdd6f84fbcdf2eded6389
TheoRmet <105194036+theormets@users.noreply.github.com>
2024-06-29 15:51:52 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,7 +1,9 @@
 from google.colab import drive
-drive.mount('/content/drive')
+
+drive.mount("/content/drive")
 
 from tensorflow.python.client import device_lib
+
 device_lib.list_local_devices()
 
 from tensorflow.keras.utils import img_to_array
@@ -28,38 +30,41 @@ os.chdir(path)
 images = []
 with os.scandir(path) as files:
     for file in files:
-        if file.name.endswith('.png'):
+        if file.name.endswith(".png"):
             images.append(file.name)
 len(images)
 
 model = VGG16()
-model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
+model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+
+
 def extract_features(file, model):
-    img = load_img(file, target_size=(224,224))
+    img = load_img(file, target_size=(224, 224))
     img = np.array(img)
-    reshaped_img = img.reshape(1,224,224,3)
+    reshaped_img = img.reshape(1, 224, 224, 3)
     imgx = preprocess_input(reshaped_img)
     features = model.predict(imgx, use_multiprocessing=True)
     return features
 
+
 data = {}
 for image in images:
-  feat = extract_features(image,model)
-  data[image] = feat
+    feat = extract_features(image, model)
+    data[image] = feat
 filenames = np.array(list(data.keys()))
 
 feat = np.array(list(data.values()))
 feat.shape
 
-feat = feat.reshape(-1,4096)
-x=feat
+feat = feat.reshape(-1, 4096)
+x = feat
 x.shape
 
 from sklearn.manifold import TSNE
-model = TSNE(n_components = 2, random_state = 0)
+
+model = TSNE(n_components=2, random_state=0)
 tsne_data = model.fit_transform(x)
-tsne_df = pd.DataFrame(data = tsne_data,
-     columns =("x", "y"))
+tsne_df = pd.DataFrame(data=tsne_data, columns=("x", "y"))
 tsne_df
 
 from sklearn.cluster import KMeans
@@ -68,7 +73,7 @@ from sklearn.datasets import make_blobs
 from yellowbrick.cluster import KElbowVisualizer
 
 model = KMeans()
-visualizer = KElbowVisualizer(model, k=(1,30))
+visualizer = KElbowVisualizer(model, k=(1, 30))
 
 visualizer.fit(tsne_df)
 visualizer.show()
@@ -81,113 +86,130 @@ from yellowbrick.cluster import KElbowVisualizer
 
 model = KMeans()
 visualizer = KElbowVisualizer(
-    model, k=(2,50), metric='calinski_harabasz', timings=False
+    model, k=(2, 50), metric="calinski_harabasz", timings=False
 )
 
 visualizer.fit(tsne_df)
 visualizer.show()
 
 wcss = []
-for i in range(1,50):
-  kmeans  = KMeans(n_clusters=i , init='k-means++', random_state=22)
-  kmeans.fit(tsne_df)
-  wcss.append(kmeans.inertia_)
+for i in range(1, 50):
+    kmeans = KMeans(n_clusters=i, init="k-means++", random_state=22)
+    kmeans.fit(tsne_df)
+    wcss.append(kmeans.inertia_)
 wcss
 
-plt.plot(range(1,50) , wcss)
+plt.plot(range(1, 50), wcss)
 
 kmeans = KMeans(n_clusters=7, random_state=22)
 kmeans.fit(tsne_df)
 
-label=kmeans.fit_predict(tsne_df)
+label = kmeans.fit_predict(tsne_df)
 kmeans.labels_
 
 from sklearn.metrics import silhouette_score
+
 score = silhouette_score(tsne_df, kmeans.labels_)
-print('Silhouetter Score: %.3f' % score)
-tsne_df['label']=label
-tsne_df.to_csv('/content/drive/df.csv', index=False)
+print("Silhouetter Score: %.3f" % score)
+tsne_df["label"] = label
+tsne_df.to_csv("/content/drive/df.csv", index=False)
 
 from matplotlib.pyplot import figure
+
 u_labels = np.unique(label)
 figure(figsize=(12, 8), dpi=80)
 for i in u_labels:
-  plt.scatter(tsne_df[label == i , 0] , tsne_df[label == i , 1] , label = i)
+    plt.scatter(tsne_df[label == i, 0], tsne_df[label == i, 1], label=i)
 plt.legend()
 plt.show()
 
 u_labels = np.unique(label)
+
+
 def graph(i):
-  plt.scatter(x[label == i , 0] , x[label == i , 1] , label = i)
-  plt.legend()
-  plt.show()
+    plt.scatter(x[label == i, 0], x[label == i, 1], label=i)
+    plt.legend()
+    plt.show()
+
+
 graph(3)
 
 u_labels = np.unique(label)
 
 for i in u_labels:
-    plt.scatter(x[label == i , 0] , x[label == i , 1] , label = i)
+    plt.scatter(x[label == i, 0], x[label == i, 1], label=i)
 plt.legend()
 plt.show()
 
-x[label == 0 , 1].shape
+x[label == 0, 1].shape
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 0.95)
+
+pca = PCA(n_components=0.95)
 pca.fit(x)
 reduced = pca.transform(x)
 reduced
 
-df1=pd.DataFrame(reduced)
+df1 = pd.DataFrame(reduced)
 df1
-df1['label']=label
+df1["label"] = label
 df1.shape
 
 u_labels = np.unique(label)
-def graph(i):
-  figure(figsize=(12, 8), dpi=80)
-  plt.scatter(reduced[label == i , 0] , reduced[label == i , 1] , label = i)
-  plt.legend()
-  plt.show()
-  print(len(label == i))
 
-df2=pd.DataFrame(reduced[label])
-df2['label']=label
-df2.to_csv('/content/drive/t3.csv', index=False)
-pd.DataFrame(reduced[label==3,0])
+
+def graph(i):
+    figure(figsize=(12, 8), dpi=80)
+    plt.scatter(reduced[label == i, 0], reduced[label == i, 1], label=i)
+    plt.legend()
+    plt.show()
+    print(len(label == i))
+
+
+df2 = pd.DataFrame(reduced[label])
+df2["label"] = label
+df2.to_csv("/content/drive/t3.csv", index=False)
+pd.DataFrame(reduced[label == 3, 0])
 graph(0)
 
 from matplotlib.pyplot import figure
+
 u_labels = np.unique(label)
 figure(figsize=(12, 8), dpi=80)
 for i in u_labels:
-  plt.scatter(reduced[label == i , 0] , reduced[label == i , 1] , label = i)
+    plt.scatter(reduced[label == i, 0], reduced[label == i, 1], label=i)
 plt.legend()
 plt.show()
 
 from sklearn.cluster import DBSCAN
-db=DBSCAN(eps=50,min_samples = 3)
-db.fit(reduced)
-label=db.labels_
 
-df=pd.DataFrame(label)
+db = DBSCAN(eps=50, min_samples=3)
+db.fit(reduced)
+label = db.labels_
+
+df = pd.DataFrame(label)
 df.value_counts()
 
 y_pred = db.fit_predict(reduced)
-plt.figure(figsize=(10,6))
-plt.scatter(reduced[:,0], reduced[:,1],c=y_pred, cmap='Paired')
+plt.figure(figsize=(10, 6))
+plt.scatter(reduced[:, 0], reduced[:, 1], c=y_pred, cmap="Paired")
 plt.title("Clusters determined by DBSCAN")
 from matplotlib import pyplot as plt
-plt.savefig('/content/drive/db.png')
+
+plt.savefig("/content/drive/db.png")
 
 u_labels = np.unique(label)
+
+
 def graph(i):
-  plt.scatter(reduced[label == i , 0] , reduced[label == i , 1] , label = i)
-  plt.legend()
-  plt.show()
+    plt.scatter(reduced[label == i, 0], reduced[label == i, 1], label=i)
+    plt.legend()
+    plt.show()
+
 
 list(label).count(-1)
 graph(-1)
 
 from sklearn.metrics import silhouette_score
+
 score = silhouette_score(reduced, label)
-print('Silhouetter Score: %.3f' % score)
+print("Silhouetter Score: %.3f" % score)
